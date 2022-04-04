d59e24e32164ad23a3d1f708609b61c9eed6e369
Aayush Katriar <aayushkatriar@gmail.com>
2022-04-04 22:36:06 +0800

    Added Randoms File

---
+++ Prac02/randoms.py
@@ -0,0 +1,7 @@
+import random
+print(random.randint(5, 20))
+print(random.randrange(3, 10, 2))
+print(random.uniform(2.5, 3.5))
+
+# Random Number in Range 1 to 100 inclusive
+print(random.randint(1, 100))
